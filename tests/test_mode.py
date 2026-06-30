a635211cb59ab9848c5d518b4d221d30ccc39034
Hong Anh Pham <phamhonganh12062000@gmail.com>
2026-06-30 12:37:45 +0700

    feat: port mode detection with tests

---
+++ tests/test_mode.py
@@ -0,0 +1,17 @@
+from antinote_qt.parse.mode import REGISTERED_KEYWORDS, detect_mode
+
+
+def test_detects_todo_with_title():
+    assert detect_mode("todo: groceries\n...") == {"keyword": "todo", "title": "groceries"}
+
+
+def test_detects_bare_todo():
+    assert detect_mode("todo") == {"keyword": "todo", "title": ""}
+
+
+def test_returns_none_for_plain_text():
+    assert detect_mode("just a note") is None
+
+
+def test_exposes_registered_keywords():
+    assert "todo" in REGISTERED_KEYWORDS
